052d7ed957819df18f8fb1b8f09a05b2cb51a35f
ravirdx <ravi.solanki146783@gmail.com>
2020-12-30 19:07:58 +0530

    ravi layout only

---
+++ Neenbo_project/app/src/main/java/com/example/neenbo_project/LikeYou.java
@@ -0,0 +1,14 @@
+package com.example.neenbo_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LikeYou extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_like_you);
+    }
+}
