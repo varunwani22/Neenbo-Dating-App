ffce4af5195e1d0af94686987675d460c5bf570f
Varun Wani <varunwani22@gmail.com>
2021-01-01 10:00:57 +0530

    latest varun Day4

--- Neenbo_project/app/src/main/java/com/example/neenbo_project/Filter.java
+++ Neenbo_project/app/src/main/java/com/example/neenbo_project/Filter.java
@@ -2,37 +2,20 @@ package com.example.neenbo_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class Filter extends AppCompatActivity implements View.OnClickListener {
+public class Filter extends AppCompatActivity {
 
-    private EditText mEtManOrWomen;
-    private EditText mEtCityAndState;
-    private EditText mEtRdius;
-    private Button mBtnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
 
-        initializeViewsAndListener();
-    }
-
-    private void initializeViewsAndListener() {
-        mEtManOrWomen = findViewById(R.id.etManOrWomen);
-        mEtCityAndState = findViewById(R.id.etCityAndState);
-        mEtRdius = findViewById(R.id.etRadius);
-        mBtnSave = findViewById(R.id.btnSave);
-
-        mBtnSave.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
 
     }
 }
